71ab560b30e33f9c75d7dde886e72eca1b63cb40
CJ Rosales <gargoth13@gmail.com>
2025-01-25 17:09:07 +0800

    Create DomeManager

--- globalgamejam2025/Scripts/GameMechanics/Dome/DomeManager.gd
+++ globalgamejam2025/Scripts/GameMechanics/Dome/DomeManager.gd
@@ -2,5 +2,31 @@ extends Node
 
 class_name DomeManager
 
-#Properties
+# Properties
+@export var domes: Array[Dome]
 
+
+# Functions
+func createDome(domeType: Enums.DomeTypeEnum, corp: Enums.DomeCorpsEnum, initPos: Vector2i) -> Dome:
+	var newDome = Dome.new(domeType, corp, initPos)
+	domes.append(newDome)
+	# TODO: Hook up to grid system to create a Node that Dome attaches to (?)
+	return newDome
+
+
+func getTotalIncome() -> int:
+	var res = 0
+	for dome in domes:
+		res += dome.getIncome()
+	return res
+
+
+func getTotalUpkeep() -> int:
+	var res = 0
+	for dome in domes:
+		res += dome.getUpkeep()
+	return res
+
+
+func countDomesByType(domeType: Enums.DomeTypeEnum) -> int:
+	return domes.filter(func(dome: Dome): return dome.type.type == domeType).size()
