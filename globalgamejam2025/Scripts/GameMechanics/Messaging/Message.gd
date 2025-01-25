e40055433dec8dbdb957657cebdbd9d3f1032001
clydejallorina <clydejallorina@gmail.com>
2025-01-25 13:23:25 +0800

    Add ability to bind functions to a message

--- globalgamejam2025/Scripts/GameMechanics/Messaging/Message.gd
+++ globalgamejam2025/Scripts/GameMechanics/Messaging/Message.gd
@@ -5,7 +5,28 @@ class_name Message
 @export var from: String
 @export var subject: String
 @export var body: String
+@export var action1: Callable = Callable()
+@export var action2: Callable = Callable()
+@export var post_read_action: Callable = Callable()
 
 func get_email_string() -> String:
 	var format = "From: %s\nSubject: %s\n---------------\n%s"
 	return format % [self.from, self.subject, self.body]
+
+func perform_action_1(args: Array) -> void:
+	if args.size() != self.action1.get_argument_count():
+		printerr("Could not run action1, mismatched arguments!")
+		return
+	self.action1.callv(args)
+
+func perform_action_2(args: Array) -> void:
+	if args.size() != self.action2.get_argument_count():
+		printerr("Could not run action1, mismatched arguments!")
+		return
+	self.action2.callv(args)
+
+func perform_post_read_action(args: Array) -> void:
+	if args.size() != self.post_read_action.get_argument_count():
+		printerr("Could not run action1, mismatched arguments!")
+		return
+	self.post_read_action.callv(args)
--- globalgamejam2025/Scripts/GameMechanics/Messaging/Messaging.gd
+++ globalgamejam2025/Scripts/GameMechanics/Messaging/Messaging.gd
@@ -7,11 +7,21 @@ func get_date() -> String:
 	var current_timestamp = Globals.STARTING_DATE + (Globals.DAY * 86400)
 	return Time.get_date_string_from_unix_time(current_timestamp)
 
-func push_message_from_data(from: String, subject: String, body: String) -> void:
+func push_message_from_data(
+	from: String,
+	subject: String,
+	body: String,
+	action1: Callable = Callable(),
+	action2: Callable = Callable(),
+	post_read_action: Callable = Callable(),
+) -> void:
 	var msg = Message.new()
 	msg.from = from
 	msg.subject = subject
 	msg.body = body
+	msg.action1 = action1
+	msg.action2 = action2
+	msg.post_read_action = post_read_action
 	Globals.message_queue.push_back(msg)
 	print("Pushed ", msg.subject, " to message queue")
 
@@ -19,10 +29,21 @@ func push_message(msg: Message) -> void:
 	Globals.message_queue.push_back(msg)
 	print("Pushed ", msg.subject, " to message queue")
 
-func pop_message() -> String:
+func pop_message_string() -> String:
+	# Use this if you only want the string for said message
 	var msg = Globals.message_queue.pop_front()
 	if msg == null:
 		printerr("No messages in queue!")
 		return "--- ERROR ---\n--- No new emails ---"
 	print("Popped ", msg.subject, " from message queue")
 	return msg.get_email_string()
+
+func pop_message() -> Message:
+	# Use this if you want to use the entire message object
+	# For example: You need to run actions from this message
+	var msg = Globals.message_queue.pop_front()
+	if msg == null:
+		printerr("No messages in queue!")
+		return null
+	print("Popped ", msg.subject, " from message queue")
+	return msg
