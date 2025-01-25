f531a0de91c50a7fa9d1d26c4c8288bedf71444d
clydejallorina <clydejallorina@gmail.com>
2025-01-25 18:41:04 +0800

    Allow multiple pop noises to happen at once

--- globalgamejam2025/Scripts/GameMechanics/GridMap/Building.gd
+++ globalgamejam2025/Scripts/GameMechanics/GridMap/Building.gd
@@ -1,6 +1,6 @@
 extends TileMapLayer
 
-@onready var pop_sound = $PopOnClick
+var ding_sound = preload("res://Assets/Audio/pop-39222.mp3")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,12 +12,15 @@ func _input(event: InputEvent) -> void:
 		var click_local_poistion = to_local(click_global_position)
 		var cell_coordinates = local_to_map(click_local_poistion)
 		if validate_coordinate(cell_coordinates):
-			pop_sound.play()
+			var audio_utils = AudioUtils.duplicate()
+			self.add_child(audio_utils)
+			audio_utils.play_sound(ding_sound)
 		print(cell_coordinates)
 
 func validate_coordinate(coordinate: Vector2i) -> bool:
 	return coordinate.x in range(Globals.GRID_SIZE[0]) and \
 	coordinate.y in range(Globals.GRID_SIZE[1])
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
---
+++ globalgamejam2025/Scripts/Utils/AudioUtils.gd
@@ -0,0 +1,16 @@
+extends AudioStreamPlayer
+
+# There has to be a better way to play multiple streams at once without
+# having to resort to this. I've checked AudioStreamPolyphonic but I'm
+# not entirely sure how good it is for this specific purpose of having
+# multiple pop noises playing at once and at different offsets.
+
+func play_sound(sound: Resource):
+	# Play a sound that automatically sets itself free once finished
+	self.stream = sound
+	self.connect("finished", self._on_audio_stream_end)
+	self.play()
+
+func _on_audio_stream_end():
+	print("Audio complete!")
+	queue_free()
