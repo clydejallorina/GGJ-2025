442d4e5691d9ea81ce0a4c6cdeb5d7c13d7dab9b
CJ Rosales <gargoth13@gmail.com>
2025-01-25 15:04:42 +0800

    feat: Implement missing Dome methods

--- globalgamejam2025/Scripts/GameMechanics/Dome/DomeType.gd
+++ globalgamejam2025/Scripts/GameMechanics/Dome/DomeType.gd
@@ -1,223 +1,98 @@
-extends Node	# Should this be a resource instead?
+extends Node  # Should this be a resource instead?
 
 class_name DomeType
 
 # Properties
 @export var type: Enums.DomeTypeEnum
-@export var domeName: String	# Might be redundant?
+@export var domeName: String  # Might be redundant?
+@export var baseCollapseChance: float
 @export var baseCost: Dictionary
 @export var baseUpkeep: Dictionary
 @export var baseIncome: Dictionary
 @export var baseBuildTime: int
 
-# TODO: Might need to add a base collapse chance here as well if it's different per dome
+# Maps to { domeName, baseCollapseChance, baseCost, baseUpkeep, baseIncome, baseBuildtime }
 var domeTypeStats = {
-	Enums.DomeTypeEnum.HOUSING: {
+	Enums.DomeTypeEnum.HOUSING:
+	{
 		"domeName": "Housing",
-		"baseCost": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseUpkeep": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseIncome": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
+		"baseCollapseChance": 0.05,
+		"baseCost": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseUpkeep": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseIncome": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
 		"baseBuildTime": 2
 	},
-	Enums.DomeTypeEnum.INDUSTRIAL: {
+	Enums.DomeTypeEnum.INDUSTRIAL:
+	{
 		"domeName": "Industrial",
-		"baseCost": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseUpkeep": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseIncome": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
+		"baseCollapseChance": 0.05,
+		"baseCost": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseUpkeep": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseIncome": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
 		"baseBuildTime": 2
 	},
-	Enums.DomeTypeEnum.MINING: {
+	Enums.DomeTypeEnum.MINING:
+	{
 		"domeName": "Mining",
-		"baseCost": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseUpkeep": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseIncome": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
+		"baseCollapseChance": 0.05,
+		"baseCost": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseUpkeep": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseIncome": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
 		"baseBuildTime": 2
 	},
-	Enums.DomeTypeEnum.LIFE_SUPPORT: {
+	Enums.DomeTypeEnum.LIFE_SUPPORT:
+	{
 		"domeName": "Life Support",
-		"baseCost": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseUpkeep": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseIncome": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
+		"baseCollapseChance": 0.05,
+		"baseCost": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseUpkeep": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseIncome": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
 		"baseBuildTime": 2
 	},
-	Enums.DomeTypeEnum.LUXURY: {
+	Enums.DomeTypeEnum.LUXURY:
+	{
 		"domeName": "Luxury",
-		"baseCost": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseUpkeep": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseIncome": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
+		"baseCollapseChance": 0.05,
+		"baseCost": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseUpkeep": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseIncome": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
 		"baseBuildTime": 2
 	},
-	Enums.DomeTypeEnum.RESEARCH: {
+	Enums.DomeTypeEnum.RESEARCH:
+	{
 		"domeName": "Research",
-		"baseCost": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseUpkeep": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseIncome": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
+		"baseCollapseChance": 0.05,
+		"baseCost": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseUpkeep": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseIncome": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
 		"baseBuildTime": 2
 	},
-	Enums.DomeTypeEnum.SPACE_ELEVATOR: {
+	Enums.DomeTypeEnum.SPACE_ELEVATOR:
+	{
 		"domeName": "Space Elevator",
-		"baseCost": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseUpkeep": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseIncome": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
+		"baseCollapseChance": 0.05,
+		"baseCost": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseUpkeep": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseIncome": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
 		"baseBuildTime": 2
 	},
-	Enums.DomeTypeEnum.WORMHOLE: {
+	Enums.DomeTypeEnum.WORMHOLE:
+	{
 		"domeName": "Wormhole",
-		"baseCost": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseUpkeep": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
-		"baseIncome": {
-			"funds": 20,
-			"life_support": 0,
-			"fuel": 0,
-			"minerals": 0,
-			"alloys": 20
-		},
+		"baseCollapseChance": 0.05,
+		"baseCost": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseUpkeep": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
+		"baseIncome": {"funds": 20, "life_support": 0, "fuel": 0, "minerals": 0, "alloys": 20},
 		"baseBuildTime": 2
 	},
 }
+
 
 # Class Constructor
 func _init(initType: Enums.DomeTypeEnum):
 	type = initType
 	domeName = domeTypeStats[initType]["domeName"]
+	baseCollapseChance = domeTypeStats[initType]["baseCollapseChance"]
 	baseCost = domeTypeStats[initType]["baseCost"]
 	baseUpkeep = domeTypeStats[initType]["baseUpkeep"]
 	baseIncome = domeTypeStats[initType]["baseIncome"]
