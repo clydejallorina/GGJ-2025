cc85a1ebd8e148eed4eec17264792ff10414327d
Chrishaneus <redelosreyes3@up.edu.ph>
2025-01-26 18:55:12 +0800

    make more ui work

---
+++ globalgamejam2025/Scripts/GameMechanics/UI/DomeCreateHandler.gd
@@ -0,0 +1,14 @@
+extends TextureButton
+
+@export var dome_type: Enums.DomeTypeEnum
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.button_down.connect(emit_build_dome)
+	
+func emit_build_dome():
+	Signals.build_dome.emit(dome_type, Enums.DomeCorpsEnum.CORP1)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
